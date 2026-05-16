ececc47cc25ea606a78b2ab6254842e2ab5e6675
Hamish Ivey-Law <hamish@ivey-law.name>
2026-05-16 13:48:57 -0400

    new files

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.NAME_VOTER_FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAAvwU9eeHVY_i4MAKeK3zP_ijd2JkeGjI",
+    authDomain: "name-voter-acd12.firebaseapp.com",
+    projectId: "name-voter-acd12",
+    storageBucket: "name-voter-acd12.firebasestorage.app",
+    messagingSenderId: "609911859719",
+    appId: "1:609911859719:web:bc3419bb7d8563d17918a2"
+};
